ea9ede4ca9a43eac42c3e5a760ee046a11b0f79e
Your Name <himanshu922@outlook.com>
2017-12-26 18:49:32 +0530

    add basic-app

--- CoreJava/basic-app/src/com/techlabs/array/TestWelcome.java
+++ CoreJava/basic-app/src/com/techlabs/array/TestWelcome.java
@@ -1,11 +1,12 @@
 package com.techlabs.array;
 
 public class TestWelcome {
-	public static void main(String args[])
-	{
-		System.out.println();
-		
+	public static void main(String names[]) {
+		if (names.length == 0) {
+			System.out.println("Enter names");
+		} else {
+			System.out.println(names.length);
+			System.out.print(names);
+		}
 	}
-	
-
 }
